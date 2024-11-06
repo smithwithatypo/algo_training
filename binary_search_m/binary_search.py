6721863bb735589556e54bbf00f95640b7907b53
smithwithatypo <smithwithatypo@gmail.com>
2024-11-06 13:39:14 -0600

    formatting binarysearch code

--- binary_search_m/binary_search.py
+++ binary_search_m/binary_search.py
@@ -1,11 +1,20 @@
+# def binary_search(arr, target):
+
+
+
+#     return -1
+
 def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
+
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        mid = (l + r) // 2
+
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
+            l = mid + 1
+        else: 
+            r = mid - 1
+
     return -1
--- binary_search_m/test_binary_search.py
+++ binary_search_m/test_binary_search.py
@@ -1,17 +1,21 @@
 import pytest
 from binary_search_m.binary_search import binary_search
 
-# def binary_search(arr, target):
-#     left, right = 0, len(arr) - 1
-#     while left <= right:
-#         mid = (left + right) // 2
-#         if arr[mid] == target:
-#             return mid
-#         elif arr[mid] < target:
-#             left = mid + 1
-#         else:
-#             right = mid - 1
-#     return -1
+''' Answer
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+'''
 
 def test_binary_search():
     # Test with an empty array
