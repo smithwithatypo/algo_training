2dbb8156191e27d18f4170caf2e6f6af3ae11997
smithwithatypo <smithwithatypo@gmail.com>
2024-11-06 14:14:00 -0600

    template for binarysearch

--- binary_search_m/binary_search.py
+++ binary_search_m/binary_search.py
@@ -1,20 +1,7 @@
-# def binary_search(arr, target):
+from typing import List
 
+def binary_search(arr: List[int], target: int) -> int:
 
-
-#     return -1
-
-def binary_search(arr, target):
-
-    l, r = 0, len(arr) - 1
-    while l <= r:
-        mid = (l + r) // 2
-
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            l = mid + 1
-        else: 
-            r = mid - 1
+    # your code here
 
     return -1
